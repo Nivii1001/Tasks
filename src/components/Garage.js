4ca3ecc6cb8ea2a36d7ee49acba194daee11657d
Nivii1001 <nivethapoopathi@gmail.com>
2025-02-10 16:57:39 +0530

    Add files via upload

---
+++ src/components/Garage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Apple from "./Apple";
+function Garage() {
+    return (
+        <>
+            <h1>Who is inside the garageeee?</h1>
+            <Apple />
+        </>
+    );
+}
+
+export default Garage;
